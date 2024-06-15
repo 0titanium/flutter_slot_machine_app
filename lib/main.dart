e337cf5dfa2213ad8742a66dbfd4e55e9899a200
kimhyungmin <jeen14rla@gmail.com>
2024-06-15 15:57:15 +0900

    chore: remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slot_machine_app/presentation/slot_machine_screen.dart';
-import 'package:flutter_slot_machine_app/slot_machine/slot_machine_game.dart';
 
 void main() {
   runApp(const SlotMachineApp());
