e04f771500046f737ca8bf0aa40680962682691a
kimhyungmin <jeen14rla@gmail.com>
2024-06-15 15:53:44 +0900

    chore: change main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slot_machine_app/presentation/slot_machine_screen.dart';
 import 'package:flutter_slot_machine_app/slot_machine/slot_machine_game.dart';
 
 void main() {
-  final game = SlotMachineGame();
-  runApp(GameWidget(game: game));
+  runApp(const SlotMachineApp());
+}
+
+class SlotMachineApp extends StatelessWidget {
+  const SlotMachineApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const SlotMachineScreen(),
+    );
+  }
 }
