5ea3cbe0c05c47e71da1503a75e506cedf057c6b
kimhyungmin <jeen14rla@gmail.com>
2024-06-21 16:05:27 +0900

    chore: add image path to const

---
+++ lib/const/const.dart
@@ -0,0 +1,4 @@
+const String sevenSymbolImagePath = 'assets/images/slot-symbol1.png';
+const String cherrySymbolImagePath = 'assets/images/slot-symbol2.png';
+const String bellSymbolImagePath = 'assets/images/slot-symbol3.png';
+const String barSymbolImagePath = 'assets/images/slot-symbol4.png';
