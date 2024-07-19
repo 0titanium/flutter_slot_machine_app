bec447aff0e29c9c13d09a77129409d228d72d15
kimhyungmin <jeen14rla@gmail.com>
2024-07-19 20:26:26 +0900

    chore: remove unused methods

--- lib/presentation/slot_machine_view_model.dart
+++ lib/presentation/slot_machine_view_model.dart
@@ -4,9 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_slot_machine_app/const/const.dart';
 
 class SlotMachineViewModel with ChangeNotifier {
-  bool _isPulled = false;
-  bool _isInit = false;
-
   String? _gameMoney;
 
   final String _sevenSymbol = sevenSymbolImagePath;
@@ -49,10 +46,6 @@ class SlotMachineViewModel with ChangeNotifier {
     barSymbolImagePath,
   ];
 
-  final List<String> _reels = [];
-
-  bool get isPulled => _isPulled;
-
   String? get gameMoney => _gameMoney;
 
   String get sevenSymbol => _sevenSymbol;
@@ -63,8 +56,6 @@ class SlotMachineViewModel with ChangeNotifier {
 
   String get barSymbol => _barSymbol;
 
-  List<String> get reels => _reels;
-
   String? get firstReelSymbol => _firstReelSymbol;
 
   String? get secondReelSymbol => _secondReelSymbol;
@@ -73,49 +64,11 @@ class SlotMachineViewModel with ChangeNotifier {
 
   List<String> get symbolList => _symbolList;
 
-  bool get isInit => _isInit;
-
   void setGameMoney() {
     _gameMoney = '1000';
   }
 
-  void setReel() {
-    int randomFirstSymbolIndex = Random().nextInt(4);
-    int randomSecondSymbolIndex = Random().nextInt(4);
-    int randomThirdSymbolIndex = Random().nextInt(4);
-
-    _firstReelSymbol = _symbolList[randomFirstSymbolIndex];
-    _secondReelSymbol = _symbolList[randomSecondSymbolIndex];
-    _thirdReelSymbol = _symbolList[randomThirdSymbolIndex];
-
-    if (isInit) {
-      _reels.addAll([firstReelSymbol!, secondReelSymbol!, thirdReelSymbol!]);
-    }
-
-    debugPrint(reels.toString());
-
-    _isInit = true;
-  }
-
-  void pullLever() {
-    _isPulled = true;
-    notifyListeners();
-
-    Future.delayed(const Duration(milliseconds: 200), () {
-      _isPulled = false;
-      notifyListeners();
-    });
-
-    setReel();
-    notifyListeners();
-
-    isMatch();
-    notifyListeners();
-  }
-
   void isMatch() {
-    if (!isInit) return;
-
     if (int.parse(_gameMoney!) < 100) {
       return;
     }
@@ -128,8 +81,6 @@ class SlotMachineViewModel with ChangeNotifier {
         firstReelSymbol == thirdReelSymbol) {
       _gameMoney = (int.parse(_gameMoney!) + 1000).toString();
 
-      _reels.clear();
-
       notifyListeners();
       return;
     }
@@ -138,12 +89,8 @@ class SlotMachineViewModel with ChangeNotifier {
         secondReelSymbol == thirdReelSymbol) {
       _gameMoney = (int.parse(_gameMoney!) + 200).toString();
 
-      _reels.clear();
-
       notifyListeners();
       return;
     }
-
-    _reels.clear();
   }
 }
